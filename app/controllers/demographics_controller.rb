a1fab4c13063bc52bd2f1d099bab2c624e468740
Brandon Lim <brandonlimchi@gmail.com>
2022-04-17 04:17:46 -0700

    sort grouped data before sending it to client side

--- app/controllers/demographics_controller.rb
+++ app/controllers/demographics_controller.rb
@@ -1,8 +1,8 @@
 class DemographicsController < ApplicationController
   def index
-    @states = obj_to_a(Demographic.group(:state).count)
-    @education = obj_to_a(Demographic.group(:education).count)
-    @estimated_income = obj_to_a(Demographic.group(:estimated_income).count)
+    @states = obj_to_a(Demographic.group(:state).count).sort
+    @education = obj_to_a(Demographic.group(:education).count).sort
+    @estimated_income = obj_to_a(Demographic.group(:estimated_income).count).sort
     @total = Demographic.count
   end
 
