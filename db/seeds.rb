a9231db906a6e656cfc3ae58136aae9de6e35f3d
Brandon Lim <brandonlimchi@gmail.com>
2022-04-17 01:08:57 -0700

    add challenge csv file and seed data to Demographic table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+CSV.foreach('./lib/seeds/coding-challenge.csv', headers: true) do |row|
+  d = Demographic.new
+  d.first_name = row['FIRST NAME']
+  d.middle_initial = row['MIDDLE INITIAL']
+  d.last_name = row['LAST NAME']
+  d.prefix = row['PREFIX']
+  d.address_1 = row['ADDRESS 1']
+  d.address_2 = row['ADDRESS 2']
+  d.city = row['CITY']
+  d.state = row['STATE']
+  d.zip_code = row['ZIP CODE']
+  d.education = row['EDUCATION']
+  d.estimated_income = row['ESTIMATED INCOME']
+  d.save
+end
