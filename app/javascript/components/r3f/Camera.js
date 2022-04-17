8cf072bbe5a48deaeb81a8f7f51c9aaeed575256
Brandon Lim <brandonlimchi@gmail.com>
2022-04-17 02:48:23 -0700

    add slight camera parallax

--- app/javascript/components/r3f/Camera.js
+++ app/javascript/components/r3f/Camera.js
@@ -1,5 +1,7 @@
 import React, { useRef } from "react";
 import { OrthographicCamera } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { Vector3 } from "three";
 
 const calcCameraZoom = () => {
   const aspect = Math.min(window.innerWidth, window.innerHeight);
@@ -9,10 +11,26 @@ const calcCameraZoom = () => {
 const Camera = (props) => {
   let cameraZoom = calcCameraZoom();
   const cameraRef = useRef();
+  const cursor = {
+    x: 0,
+    y: 0
+  };
+  const parallaxAmplitude = 1.8;
+
+  window.addEventListener('mousemove', (e) => {
+    cursor.x = (e.clientX / window.innerWidth) - 0.5;
+    cursor.y = (e.clientY / window.innerHeight) - 0.5;
+  });
 
   window.addEventListener('resize', () => {
     cameraZoom = calcCameraZoom();
     cameraRef.current.zoom = cameraZoom;
+  });
+
+  useFrame(() => {
+    cameraRef.current.position.x = cursor.x * parallaxAmplitude;
+    cameraRef.current.position.y = -(cursor.y * parallaxAmplitude);
+    cameraRef.current.lookAt(new Vector3(0, 0, 0));
   });
 
   return (
