831dcbc3b2e149adcff0900f91690220b835b5fe
Brandon Lim <brandonlimchi@gmail.com>
2022-04-17 01:40:34 -0700

    format data on server and pass to react client side

--- app/controllers/demographics_controller.rb
+++ app/controllers/demographics_controller.rb
@@ -1,5 +1,16 @@
 class DemographicsController < ApplicationController
   def index
-    @test = 'route test'
+    @states = obj_to_a(Demographic.group(:state).count)
+    @education = obj_to_a(Demographic.group(:education).count)
+    @estimated_income = obj_to_a(Demographic.group(:estimated_income).count)
+    @total = Demographic.count
+  end
+
+  private
+  
+  def obj_to_a(obj)
+    obj.map do |key, val|
+      [key, val]
+    end
   end
 end
--- app/javascript/components/DataVisual.js
+++ app/javascript/components/DataVisual.js
@@ -1,13 +1,18 @@
 import React from "react";
 
 const DataVisual = (props) => {
-  console.log(props.greeting)
+  const { states, education, income, total } = props;
+  const data = {
+    states: states,
+    education: education,
+    income: income
+  };
 
   return (
     <div>
       react-rails test react-rails test react-rails test 
     </div>
-  )
+  );
 };
 
 export default DataVisual;
