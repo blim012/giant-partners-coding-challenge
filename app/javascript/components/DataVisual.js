a5832e832b4e49df69b48aeea14322d781609f09
Brandon Lim <brandonlimchi@gmail.com>
2022-04-17 01:26:59 -0700

    install react-rails and create test component

---
+++ app/javascript/components/DataVisual.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const DataVisual = (props) => {
+  console.log(props.greeting)
+
+  return (
+    <div>
+      react-rails test react-rails test react-rails test 
+    </div>
+  )
+};
+
+export default DataVisual;
